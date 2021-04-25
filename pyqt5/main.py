7fd85bd18db6e1563768f484a47db59fbcd43422
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-04-25 18:38:11 -0300

    New images and queries

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -6,22 +6,12 @@ def albedo():
     conn = sqlite3.connect('db/genshin.db')
     c = conn.cursor()
 
-    tela.lb_weapon_1.setPixmap(QtGui.QPixmap('img/weapons/Skyward_Blade.png'))
-    tela.lb_weapon_2.setPixmap(QtGui.QPixmap('img/weapons/Festering_Desire.png'))
-    tela.lb_artifact_1.setPixmap(QtGui.QPixmap('img/artifact/archaic_petra.png'))
-    tela.lb_artifact_2.setPixmap(QtGui.QPixmap('img/artifact/noblesse_oblige.png'))
-    tela.lb_weapon_name_1.setText('Skyward Blade')
-    tela.lb_weapon_name_2.setText('Festering Desire')
-    tela.lb_artifact_name_1.setText('Archaic Petra')
-    tela.lb_artifact_name_2.setText('Noblesse Oblige')
+    #tela.lb_background.setPixmap
 
 
 app = QtWidgets.QApplication([])
 
-tela = uic.loadUi('ui/tela.ui')
-
-tela.lb_dps_build.setPixmap(QtGui.QPixmap('img/dps_build.png'))
-tela.lb_support_build.setPixmap(QtGui.QPixmap('img/support_build.png'))
+tela = uic.loadUi('ui/tela_inicial.ui')
 
 tela.actionExit.triggered.connect(lambda: exit())
 tela.actionAlbedo.triggered.connect(albedo)
