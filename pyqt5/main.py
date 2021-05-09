9fd276c4fa030fb9fd9eb556f01908f418209d02
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-05-09 16:46:44 -0300

    changes

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -9,7 +9,7 @@ tela = uic.loadUi('ui/tela.ui')
 tela.lb_dps_build.setPixmap(QtGui.QPixmap('img/dps_build.png'))
 
 tela.actionExit.triggered.connect(lambda: exit())
-tela.actionAlbedo.triggered.connect(images.characters.checar)
+tela.actionAlbedo.triggered.connect(checar)
 
 tela.show()
 app.exec_()
