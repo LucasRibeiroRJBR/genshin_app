61bc6a48ce30b9cde604cd8728069271bcd9d12e
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-05-14 18:10:02 -0300

    Layout adjustments

--- pyqt5/main.py
+++ pyqt5/main.py
@@ -1,16 +1,16 @@
 from PyQt5 import uic, QtWidgets, QtGui
 from util.images import element
 
-chars = ['Albedo','Amber','Barbara','Beidou','Bennett','Chongyun','Diluc','Diona','Fischl','Ganyu','Hu Tao','Jean','Kaeya','Keqing','Klee','Lisa','Mona','Ningguang','Noelle','Qiqi','Razor','Sucrose','Tartaglia','Venti','Xiangling','Xiao','Xingqiu','Xinyan','Zhongli']
+chars = ['Albedo','Amber','Barbara','Beidou','Bennett','Chongyun','Diluc','Diona','Fischl','Ganyu','Hu Tao','Jean','Kaeya','Keqing','Klee','Lisa','Mona','Ningguang','Noelle','Qiqi','Razor','Sucrose','Tartaglia','Venti','Xiangling','Xiao','Xingqiu','Xinyan','Yanfei','Zhongli']
 
-chars2 = ['Albedo','Amber','Barbara','Beidou_4','Bennett','Chongyun','Diluc','Diona','Fischl_3','Ganyu','Hu_Tao','Jean','Kaeya','Keqing_3','Klee','Lisa_2','Mona','Ningguang','Noelle','Qiqi','Razor_3','Sucrose','Tartaglia','Venti','Xiangling','Xiao','Xingqiu','Xinyan','Zhongli']
+chars2 = ['Albedo','Amber','Barbara','Beidou_4','Bennett','Chongyun','Diluc','Diona','Fischl_3','Ganyu','Hu_Tao','Jean','Kaeya','Keqing_3','Klee','Lisa_2','Mona','Ningguang','Noelle','Qiqi','Razor_3','Sucrose','Tartaglia','Venti','Xiangling','Xiao','Xingqiu','Xinyan','Yanfei','Zhongli']
 
 
 app = QtWidgets.QApplication([])
-
 tela = uic.loadUi('ui/tela.ui')
 
 tela.lb_dps_build.setPixmap(QtGui.QPixmap('img/dps_build.png'))
+tela.lb_sup_build.setPixmap(QtGui.QPixmap('img/sup_build.png'))
 
 tela.actionExit.triggered.connect(lambda: exit())
 
